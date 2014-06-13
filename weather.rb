f51252e0ed3bf5ea5a5aec965ce34f7e4040846f
Mike Chandler <michael.chandler@onassignment.com>
2014-06-13 12:01:43 -0700

    Learning to read.

--- weather.rb
+++ weather.rb
@@ -1,6 +1,6 @@
 file = File.open("w_data.dat", "r");
-day = 0
-max_spread = 0
+days = Array.new
+all_spreads = Array.new
 
 file.each_line do |line|
 
@@ -8,16 +8,11 @@ file.each_line do |line|
 
   if line_array[0].to_i > 0
 
-    test_day = line_array[0].to_i
-    spread = line_array[1].to_i - line_array[2].to_i
-    
-    if spread > max_spread
-      day = test_day
-      max_spread = spread
-    end
+    days.push line_array[0]
+    all_spreads.push line_array[1].to_i - line_array[2].to_i
     
   end
 end
 
 file.close
-puts "Day " + day.to_s
+puts "Day " + days[all_spreads.each_with_index.min.last]
